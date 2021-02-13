b6a8bcad15bbfeff568d3ae99d3dd77a77a2abd5
leshawn-rice <leshawnrice3@gmail.com>
2021-02-13 00:40:14 -0800

    initial commit

---
+++ readme.md
@@ -0,0 +1,25 @@
+# Jobly Backend
+
+This is the Express backend for Jobly, version 2.
+
+To seed database:
+    
+    npm run seed
+
+To run this:
+
+    npm start
+       or
+    node server.js
+
+To run in developer mode:
+
+    npm run dev
+       or
+    nodemon server.js
+    
+To run the tests:
+
+    npm run test
+       or
+    jest -i
---
+++ routes/companies.js
@@ -0,0 +1,138 @@
+"use strict";
+
+/** Routes for companies. */
+
+const jsonschema = require("jsonschema");
+const express = require("express");
+
+const { BadRequestError } = require("../expressError");
+const { ensureAdmin } = require("../middleware/auth");
+const Company = require("../models/company");
+const Job = require('../models/job');
+
+const companyNewSchema = require("../schemas/companyNew.json");
+const companyUpdateSchema = require("../schemas/companyUpdate.json");
+
+const router = new express.Router();
+
+
+/** POST / { company } =>  { company }
+ *
+ * company should be { handle, name, description, numEmployees, logoUrl }
+ *
+ * Returns { handle, name, description, numEmployees, logoUrl }
+ *
+ * Authorization required: login
+ */
+
+router.post("/", ensureAdmin, async function (req, res, next) {
+  try {
+    const validator = jsonschema.validate(req.body, companyNewSchema);
+    if (!validator.valid) {
+      const errs = validator.errors.map(e => e.stack);
+      throw new BadRequestError(errs);
+    }
+
+    const company = await Company.create(req.body);
+    return res.status(201).json({ company });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** GET /  =>
+ *   { companies: [ { handle, name, description, numEmployees, logoUrl }, ...] }
+ *
+ * Can filter on provided search filters:
+ * - minEmployees
+ * - maxEmployees
+ * - name (will find case-insensitive, partial matches)
+ *
+ * Authorization required: none
+ */
+
+router.get("/", async function (req, res, next) {
+  try {
+    // Get filtering query params
+    let { name, minEmployees, maxEmployees } = req.query;
+    minEmployees = parseInt(minEmployees);
+    maxEmployees = parseInt(maxEmployees);
+
+    if (minEmployees > maxEmployees) {
+      throw new BadRequestError("minEmployees cannot be greater than maxEmployees!");
+    }
+
+    // We need to only send the query params that we received
+    let dataToSend = {}
+    if (name) dataToSend.name = name;
+    if (minEmployees) dataToSend.minEmployees = minEmployees;
+    if (maxEmployees) dataToSend.maxEmployees = maxEmployees;
+
+    const companies = await Company.findAll(dataToSend);
+    return res.json({ companies });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** GET /[handle]  =>  { company }
+ *
+ *  Company is { handle, name, description, numEmployees, logoUrl, jobs }
+ *   where jobs is [{ id, title, salary, equity }, ...]
+ *
+ * Authorization required: none
+ */
+
+router.get("/:handle", async function (req, res, next) {
+  try {
+    const company = await Company.get(req.params.handle);
+    const jobs = await Job.getByCompanyHandle(req.params.handle);
+    company.jobs = jobs;
+    return res.json({ company });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** PATCH /[handle] { fld1, fld2, ... } => { company }
+ *
+ * Patches company data.
+ *
+ * fields can be: { name, description, numEmployees, logo_url }
+ *
+ * Returns { handle, name, description, numEmployees, logo_url }
+ *
+ * Authorization required: admin
+ */
+
+router.patch("/:handle", ensureAdmin, async function (req, res, next) {
+  try {
+    const validator = jsonschema.validate(req.body, companyUpdateSchema);
+    if (!validator.valid) {
+      const errs = validator.errors.map(e => e.stack);
+      throw new BadRequestError(errs);
+    }
+
+    const company = await Company.update(req.params.handle, req.body);
+    return res.json({ company });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** DELETE /[handle]  =>  { deleted: handle }
+ *
+ * Authorization: admin
+ */
+
+router.delete("/:handle", ensureAdmin, async function (req, res, next) {
+  try {
+    await Company.remove(req.params.handle);
+    return res.json({ deleted: req.params.handle });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+
+module.exports = router;
